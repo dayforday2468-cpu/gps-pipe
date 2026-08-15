76955ad08f59309b08fc1a10a850c809a10523cd
Myeongcheol Kim <dayforday@kaist.ac.kr>
2026-08-15 17:27:05 +0900

    refactor: add semantic GPS batch loaders

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from modules.dataload import load_raw_positions
+from modules.dataload import *
 
+from modules.logger import get_logger
+
+logger = get_logger(__name__)
+data_path = "data/timeline.json"
 
 if __name__=="__main__":
-    raw_positions_df = load_raw_positions("data/timeline.json")
+    # 데이터 배치 로더 생성v
+    raw_positions_batches = load_raw_positions_batches(data_path)
+    logger.info("raw position loader initialized")
 
-    for df in raw_positions_df:
-        print(df)
+    timeline_paths_batches = load_timeline_paths_batches(data_path)
+    logger.info("timeline path loader initialized")
+
+    visits_batches = load_visits_batches(data_path)
+    logger.info("visit loader initialized")
+
+    activities_batches = load_activities_batches(data_path)
+    logger.info("activity loader initialized")
--- modules/dataload.py
+++ modules/dataload.py
@@ -5,7 +5,7 @@ from collections.abc import Iterator
 from modules.decorators import *
 
 
-def extract_raw_segments(path: str, key: str):
+def _extract_raw_segments(path: str, key: str):
     with open(path, "rb") as f:
         for segment in ijson.items(f, "rawSignals.item"):
             value = segment.get(key)
@@ -13,8 +13,23 @@ def extract_raw_segments(path: str, key: str):
             if value is not None:
                 yield value
 
+def _extract_semantic_segments(path: str, key: str):
+    with open(path, "rb") as f:
+        for segment in ijson.items(
+            f,
+            "semanticSegments.item",
+        ):
+            value = segment.get(key)
 
-def batch_records(records, batch_size: int):
+            if value is not None:
+                yield {
+                    "startTime": segment["startTime"],
+                    "endTime": segment["endTime"],
+                    key: value,
+                }
+
+
+def _batch_records(records, batch_size: int):
     batch = []
 
     for record in records:
@@ -29,17 +44,17 @@ def batch_records(records, batch_size: int):
 
 
 @measure_generator_time
-def load_raw_positions(
+def load_raw_positions_batches(
     path: str,
     batch_size: int = 10000,
 ) -> Iterator[pl.DataFrame]:
 
-    records = extract_raw_segments(
+    records = _extract_raw_segments(
         path,
         key="position",
     )
 
-    for batch in batch_records(records, batch_size):
+    for batch in _batch_records(records, batch_size):
         df = (
             pl.DataFrame(batch)
             .with_columns(
@@ -74,6 +89,241 @@ def load_raw_positions(
                 "longitude",
                 pl.col("accuracyMeters").alias("accuracy"),
                 "timestamp",
+            )
+        )
+
+        yield df
+
+@measure_generator_time
+def load_timeline_paths_batches(
+    path: str,
+    batch_size: int = 10_000,
+) -> Iterator[pl.DataFrame]:
+
+    records = _extract_semantic_segments(
+        path,
+        key="timelinePath",
+    )
+
+    for batch in _batch_records(records, batch_size):
+        df = (
+            pl.DataFrame(batch)
+            .explode("timelinePath")
+            .with_columns(
+                pl.col("timelinePath")
+                .struct.field("point")
+                .str.split_exact(",", 1)
+                .alias("coordinates"),
+
+                pl.col("timelinePath")
+                .struct.field("time")
+                .str.to_datetime(
+                    format="%Y-%m-%dT%H:%M:%S%.3f%:z"
+                )
+                .dt.convert_time_zone("Asia/Seoul")
+                .alias("timestamp"),
+            )
+            .with_columns(
+                pl.col("coordinates")
+                .struct.field("field_0")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("latitude"),
+
+                pl.col("coordinates")
+                .struct.field("field_1")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("longitude"),
+            )
+            .select(
+                "latitude",
+                "longitude",
+                "timestamp",
+            )
+        )
+
+        yield df
+
+@measure_generator_time
+def load_visits_batches(
+    path: str,
+    batch_size: int = 10_000,
+) -> Iterator[pl.DataFrame]:
+
+    records = _extract_semantic_segments(
+        path,
+        key="visit",
+    )
+
+    for batch in _batch_records(records, batch_size):
+        df = (
+            pl.DataFrame(batch)
+            .with_columns(
+                pl.col("visit")
+                .struct.field("topCandidate")
+                .struct.field("placeLocation")
+                .struct.field("latLng")
+                .str.split_exact(",", 1)
+                .alias("coordinates"),
+
+                pl.col("startTime")
+                .str.to_datetime(
+                    format="%Y-%m-%dT%H:%M:%S%.3f%:z"
+                )
+                .dt.convert_time_zone("Asia/Seoul")
+                .alias("start_time"),
+
+                pl.col("endTime")
+                .str.to_datetime(
+                    format="%Y-%m-%dT%H:%M:%S%.3f%:z"
+                )
+                .dt.convert_time_zone("Asia/Seoul")
+                .alias("end_time"),
+            )
+            .with_columns(
+                pl.col("coordinates")
+                .struct.field("field_0")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("latitude"),
+
+                pl.col("coordinates")
+                .struct.field("field_1")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("longitude"),
+
+                pl.col("visit")
+                .struct.field("probability")
+                .alias("probability"),
+
+                pl.col("visit")
+                .struct.field("topCandidate")
+                .struct.field("semanticType")
+                .alias("semantic_type"),
+
+                pl.col("visit")
+                .struct.field("topCandidate")
+                .struct.field("probability")
+                .alias("place_probability"),
+            )
+            .select(
+                "start_time",
+                "end_time",
+                "latitude",
+                "longitude",
+                "semantic_type",
+                "probability",
+                "place_probability",
+            )
+        )
+
+        yield df
+
+@measure_generator_time
+def load_activities_batches(
+    path: str,
+    batch_size: int = 10_000,
+) -> Iterator[pl.DataFrame]:
+
+    records = _extract_semantic_segments(
+        path,
+        key="activity",
+    )
+
+    for batch in _batch_records(records, batch_size):
+        df = (
+            pl.DataFrame(batch)
+            .with_columns(
+                pl.col("activity")
+                .struct.field("start")
+                .struct.field("latLng")
+                .str.split_exact(",", 1)
+                .alias("start_coordinates"),
+
+                pl.col("activity")
+                .struct.field("end")
+                .struct.field("latLng")
+                .str.split_exact(",", 1)
+                .alias("end_coordinates"),
+
+                pl.col("startTime")
+                .str.to_datetime(
+                    format="%Y-%m-%dT%H:%M:%S%.3f%:z"
+                )
+                .dt.convert_time_zone("Asia/Seoul")
+                .alias("start_time"),
+
+                pl.col("endTime")
+                .str.to_datetime(
+                    format="%Y-%m-%dT%H:%M:%S%.3f%:z"
+                )
+                .dt.convert_time_zone("Asia/Seoul")
+                .alias("end_time"),
+            )
+            .with_columns(
+                pl.col("start_coordinates")
+                .struct.field("field_0")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("start_latitude"),
+
+                pl.col("start_coordinates")
+                .struct.field("field_1")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("start_longitude"),
+
+                pl.col("end_coordinates")
+                .struct.field("field_0")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("end_latitude"),
+
+                pl.col("end_coordinates")
+                .struct.field("field_1")
+                .str.replace("°", "")
+                .str.strip_chars()
+                .cast(pl.Float64)
+                .alias("end_longitude"),
+
+                pl.col("activity")
+                .struct.field("distanceMeters")
+                .alias("distance"),
+
+                pl.col("activity")
+                .struct.field("probability")
+                .alias("probability"),
+
+                pl.col("activity")
+                .struct.field("topCandidate")
+                .struct.field("type")
+                .alias("activity_type"),
+
+                pl.col("activity")
+                .struct.field("topCandidate")
+                .struct.field("probability")
+                .alias("activity_probability"),
+            )
+            .select(
+                "start_time",
+                "end_time",
+                "start_latitude",
+                "start_longitude",
+                "end_latitude",
+                "end_longitude",
+                "distance",
+                "activity_type",
+                "probability",
+                "activity_probability",
             )
         )
 
